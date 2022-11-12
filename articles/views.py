c5e78af5409ec0edf8a0cb11d996e78080a24e3c
nagah <nagah.shinawy@gmail.com>
2022-11-12 14:33:47 +0200

    using IsAuthenticated

--- articles/views.py
+++ articles/views.py
@@ -1,63 +1,33 @@
-from rest_framework import generics, status
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 
 from .models import Article, Author
 from .serializers import ArticleSerializer
 
 
-class ArticleQueryMixin:
-
-    request = None
-
-    def get_queryset(self):
-        if self.request.user.is_authenticated:
-            return Article.objects.filter(author__user=self.request.user)
-        return False
-
-
 class CreateArticleAPIView(generics.CreateAPIView):
     serializer_class = ArticleSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user.author)
 
 
-class ListArticleAPIView(ArticleQueryMixin, generics.ListAPIView):
+class ListArticleAPIView(generics.ListAPIView):
     serializer_class = ArticleSerializer
-
-    def list(self, request, *args, **kwargs):
-        if self.get_queryset() is False:
-            return Response(
-                data={"message": "Not Allowed"}, status=status.HTTP_403_FORBIDDEN
-            )
-        return super().list(request, *args, **kwargs)
+    queryset = Article.objects.all()
 
 
-class DetailArticleAPIView(ArticleQueryMixin, generics.RetrieveAPIView):
+class DetailArticleAPIView(generics.RetrieveAPIView):
     serializer_class = ArticleSerializer
-
-    def get(self, request, *args, **kwargs):
-        if self.get_queryset() is False:
-            return Response(
-                data={"message": "Not Allowed"}, status=status.HTTP_403_FORBIDDEN
-            )
-        return super().get(request, *args, **kwargs)
+    queryset = Article.objects.all()
 
 
-class UpdateArticleAPIView(ArticleQueryMixin, generics.UpdateAPIView):
+class UpdateArticleAPIView(generics.UpdateAPIView):
     serializer_class = ArticleSerializer
-
-    def update(self, request, *args, **kwargs):
-        if self.get_queryset() is False:
-            return Response(
-                data={"message": "Not Allowed"}, status=status.HTTP_403_FORBIDDEN
-            )
-        return super().update(request, *args, **kwargs)
+    queryset = Article.objects.all()
 
 
-class DeleteArticleAPIView(ArticleQueryMixin, generics.DestroyAPIView):
+class DeleteArticleAPIView(generics.DestroyAPIView):
     serializer_class = ArticleSerializer
-
-    def delete(self, request, *args, **kwargs):
-        if self.get_queryset() is False:
-            return Response(
-                data={"message": "Not Allowed"}, status=status.HTTP_403_FORBIDDEN
-            )
-        return super().delete(request, *args, **kwargs)
+    queryset = Article.objects.all()
